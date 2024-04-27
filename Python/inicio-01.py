286bd4c8b99e952fd6bec717209a67b684ac9221
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-04-27 20:43:10 -0300

    Agregando como nombrar una variable

--- Python/inicio-01.py
+++ Python/inicio-01.py
@@ -4,7 +4,7 @@
 
 #Así podemos ver el tipo de dato del texto, y los tipos de datos:
 
-print(type("Hola python"));
+print(type("Hola python"))
 print(type(23))
 print(type(23.3))
 print(type(1+3j))
--- Python/variables-02.py
+++ Python/variables-02.py
@@ -1,2 +1,18 @@
 #Variables en python
 
+#Siempre nombramos las variables con la tipografia "Snake_Case".
+
+my_variable="My string variable"
+
+print(my_variable); 
+
+
+first_name="Nicolas"
+last_name="Diaz"
+age=19
+country="Argentina"
+city="Maipu"
+
+print(first_name,last_name,age,country,city)
+
+
