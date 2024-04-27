4138491f6f78a9c3ad206943cd4f2e4060ec275f
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-04-27 20:26:29 -0300

    Inicio del curso de python hasta experto

---
+++ Python/inicio-01.py
@@ -0,0 +1,12 @@
+#!!! INICIO DEL CURSO DE PYTHON HASTA EXPERTO EN MACHINE LEARNING,DEEP LEARNING Y COMPUTER VISION Y TAMBIÉN FRAMEWORKS Y LIBRERIAS !!!
+
+#Esto es un comentario hecho en python
+
+#Así podemos ver el tipo de dato del texto, y los tipos de datos:
+
+print(type("Hola python"));
+print(type(23))
+print(type(23.3))
+print(type(1+3j))
+print(type([1,2,3]))
+
