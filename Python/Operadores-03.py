cea3f40d267cec3fddafc9fee8cf3908147ae9b8
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-04-28 12:47:51 -0300

    Aprendiendo los tipos de operadores en python

---
+++ Python/Operadores-03.py
@@ -0,0 +1,29 @@
+#Operadores en python 
+
+#Operadores aritmetricos:
+
+print(6+10) #Operador de suma
+print(10-6) #Operador de resta
+print(10*6) #Operador de multiplicacion
+print(6/10) #Operador de division
+print(10%6) #Operador de modulo
+print(10//6) #Operador de flor division, tiene que llegar a un número entero
+print(10**6) #Operador para la base
+ 
+ 
+print("Hola"+"python"+"Que tal?")
+print("Hola"+str(5))
+print("Hola"*5)
+print("Hola"*(2**5))
+
+my_float=2.5*2
+
+print("Hola" * int(my_float))
+
+#Operadores comparativos:
+
+print(3==5) #Operador de igualdad
+print(3>4)  #Operador de mayor
+print(3<4)  #Operador de menor
+print(3>=4) #Operador de mayor igual
+print(3<=5) #Operador de menor igual
