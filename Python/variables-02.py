58d352e5900bd15010b96ed49caeeefb7d993418
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-04-28 12:05:28 -0300

    Vimos los diferentes tipos de variables

--- Python/variables-02.py
+++ Python/variables-02.py
@@ -13,6 +13,31 @@ age=19
 country="Argentina"
 city="Maipu"
 
+#Concatenación de variables en un print
 print(first_name,last_name,age,country,city)
+
+#Funciones del sistema
+
+print(len(first_name))
+
+# Variables en una sola linea
+# name,surname,alias,age="Nico","Diaz","Mocho",19
+
+# print(name,surname,age," y mi alias es: ",alias)
+
+
+#Forzamos el tipo
+
+ubicacion:str="Mi direccion"
+ubicacion=23
+
+print(type(ubicacion))
+
+
+
+
+
+
+
 
 
