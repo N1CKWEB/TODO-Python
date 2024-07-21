abfb70a396411221b8d6bd822d32fbc8ead8cca7
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-07-21 11:37:17 -0300

    Aprendiendo a usar sobrecarga de operadores en python

--- Python/Sección-11/Sobre_carga.py
+++ Python/Sección-11/Sobre_carga.py
@@ -1,2 +1,32 @@
 # Sobrecarga de operadores en python
 
+class Persona:
+    def __init__(self, nombre,edad):
+      self.nombre = nombre
+      self.edad = edad
+    
+    def __add__(self,otro):
+        return f"{self.nombre} {otro.nombre}"
+      
+    def __sub__(self,otro):
+        return self.edad -  otro.edad
+      
+    
+ 
+persona1=Persona("Nicolas",19)
+
+persona2=Persona("Ariel",29) 
+
+print(persona1+persona2)   
+print(persona1-persona2)   
+    
+
+
+           
+        
+
+
+
+
+
+
