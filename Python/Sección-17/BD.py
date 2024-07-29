0ef4e2014c67c79400830f89c1d581e1a25596d7
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-07-29 15:15:50 -0300

    Aprendimos a usar Python + PostgreSQL para usar bases de datos

---
+++ Python/Sección-17/BD.py
@@ -0,0 +1,44 @@
+
+# Un cursor nos permite ejecutar sentencia SQL en postgreSQL 
+import psycopg2
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='admin', 
+    host='127.0.0.1',
+    port='5432',
+    database='test_db'
+    )
+
+
+# Función fetch all en Pyscopg
+try:
+     with conexion:
+      with conexion.cursor() as cursor:
+       sentencia="DELETE FROM persona WHERE id_persona IN %s "
+       entrada=input("Proporciona los id personas a (eliminar): ")
+       valores=(tuple(entrada.split(",")),)
+       cursor.execute(sentencia,valores)
+     #   conexion.commit() #Guarda los cambios en la BD
+       registros_actualizados=cursor.rowcount
+       print(f"Registros eliminados: {registros_actualizados}") 
+         
+     #   sentencia = 'SELECT * FROM persona WHERE id_persona IN %s'
+     #   entrada = input("Proporciona los id\'s a buscar (separados por comas): ")
+     #   llaves_primarias=(tuple(entrada.split(',')),)
+     # #   id_persona = input("Proporciona el valor id_persona: ")
+     #   cursor.execute(sentencia,llaves_primarias)
+     # # Acá recuperamos todos los registros con esta sentencia
+     # #   registros = cursor.fetchall() #este metodo recupera todos los  registros que se han recuperado
+     #   registros=cursor.fetchall() #Lo que hace que recuperar solo un registro de lo pedido
+     #   for registro in registros:
+     #        print(registros)   
+     #   print(registros) 
+     
+except Exception as e:
+     print(f'Ocurrio un error {e}')
+finally:     
+     conexion.close()
+  
+
+
