e157a57d6d3bb3b96f907173c2208c5314e945bb
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-08-08 19:28:52 -0300

    Concatenacion de str, viendo metodos help y __doc__ para todo relacionado con los Strings

---
+++ Python/Sección-21/Concatenacion_str.py
@@ -0,0 +1,37 @@
+from Mi_clase import MiClase
+
+
+# Profundizando en el tipo str
+
+# Concatenación automatica 
+
+mensaje="Hola" "Mundo"
+mensaje +="Universidad" "Python"
+print(mensaje)
+
+
+
+# Solicitar ayuda (método help), nos ayuda para poder buscar cualquier tipo de dato en python, ya sea un modulo o libreria, método,clases o lo que sea
+
+
+
+help(str.capitalize)
+
+
+# Uso de docstring en python:
+
+help(MiClase)
+
+# El método __doc__  llama la documentación de la clase y la definición de la clase
+print(MiClase.__doc__)
+print(MiClase.__init__.__doc__)
+print(MiClase.mi_metodo.__doc__)
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
+
+'''
+
+'''
+
+
+
---
+++ Python/Sección-21/Mi_clase.py
@@ -0,0 +1,25 @@
+
+
+class MiClase:
+   '''
+   Este es un ejemplo de la documentación de nuestra clase
+   '''
+   
+   def __init__(self, name, age):
+     '''
+     Método de inicio de nuestra clase
+
+     '''
+
+   def mi_metodo(args,param1,param2):
+     '''
+      Esta es la documentación de nuestro método
+     :Param: param1:Este es el paramentro 1
+     :Param param2:Este es el paramentro 2
+     :return:Este el retorno de los parametros
+     '''
+
+
+
+
+
