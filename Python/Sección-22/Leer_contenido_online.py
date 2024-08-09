1f5dbd7cf0f04125000521f0ae530b364fe76dfc
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-08-09 14:52:23 -0300

    Aprendimos manejo de literales de tipo byte, métodos,funciones,formatos y mucho más

---
+++ Python/Sección-22/Leer_contenido_online.py
@@ -0,0 +1,55 @@
+
+from urllib.request import urlopen
+
+
+# Leer el contenido online
+
+# with urlopen('http://globalmentoring.com.mx/recuros/GlobalMentoring.txt') as mensaje:
+# #   contenido=mensaje.read()    
+#   contenido=mensaje.read().decode('UTF-8')
+#   print(contenido)
+
+with open('pythonIA.txt','r',encoding='UTF-8') as archivo:
+    contenido=archivo.read()
+
+
+# Otros métodos de la clase (str)
+
+# Contar ocurrencia de una cadena de otra
+
+print(contenido.count('Python'))
+
+
+# Método upper
+
+# print(contenido.upper())
+
+# # Método lower
+
+# print(contenido.lower())
+
+
+# Vamos a preguntas si esta cadena existe
+
+# print('python'.lower() in contenido.lower())
+
+
+# srtatswith - inica con
+print('Inicia con',contenido.startswith('P'))
+
+# endswith - termina con
+print('Termina con',contenido.upper().endswith(' lenguaje natural.'.upper()))
+
+
+# Funciones islower y isupper en python, significan los dos si contiene todos los caracteres en minisculas(islower) y mayusculas(isupper)
+
+mensaje='Hola Mundo'
+print(mensaje.lower().islower())
+print(mensaje.upper().isupper())
+
+
+
+
+
+
+
--- Python/Sección-22/Manejo_de_literales_Byte.py
+++ Python/Sección-22/Manejo_de_literales_Byte.py
@@ -1,2 +1,48 @@
 # Manejo de Literales de tipo Byte en python
 
+
+
+# Caracteres en bytes
+
+caracteres_en_bytes=b"Hola Mundo"
+print(caracteres_en_bytes)
+
+
+mensaje=b"Universidad Python"
+
+print(mensaje[0])
+
+print(chr(mensaje[0]))
+
+
+lista_caracteres=mensaje.split()
+print(lista_caracteres)
+
+
+# Convertir de str a bytes y viceversa
+
+string = 'Programación con Python'
+
+print(f'String original {string}')
+
+# Lo que hace el método encode que codifica una cadena para convertirla en otro tipo de dato
+
+# Convertir en string a bytes usando encode
+
+bytes=string.encode('UTF-8')
+
+print(f"Bytes codificado: {bytes}")
+
+# Convertir en bytes a string usando decode
+
+#Lo que hace el método decode que decodifca una cadena para conrvetirla en el mismo tipo de dato que tenia 
+
+string2=bytes.decode('UTF-8')
+
+print(f"String decodificado: {string2}")
+
+
+
+print(string == string2)
+
+
