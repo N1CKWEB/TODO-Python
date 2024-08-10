c833ec381d47a61c856c05007fcd596aebacc657
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-08-10 12:31:16 -0300

    Profundizando en listas

---
+++ Python/Sección-25/Profundizando_en_Listas.py
@@ -0,0 +1,27 @@
+
+
+
+# Profundizando en listas en python
+# Listas son mutables, signifiica que se pueden modificar
+
+
+nombres=['Nicolás','Juan','Naim','Gordo']
+nombres2='Sergio Mariela Olga  Priscila Lautaro'.split()
+
+# Sumar una lista
+ 
+print(f"Suma de lista {nombres+nombres2}")
+
+# Extender una lista con otra lista
+
+nombres.extend(nombres2)
+print(f"Extender la lista: {nombres}")
+
+
+
+# Profundizando en listas - segunda parte
+
+
+
+
+
