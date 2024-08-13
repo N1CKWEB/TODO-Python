c9fdd79c598165dae1ce761fe8dbf53b41b41023
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-08-13 14:44:29 -0300

    Aprendimos más en tuplas, digamos profundizamos

--- Python/Sección-28/Profundizando_en_Tuplas.py
+++ Python/Sección-28/Profundizando_en_Tuplas.py
@@ -1,9 +1,45 @@
 
 # Profundizando en tuplas en python
 
+# Podemos usar tuplas para declarar variables
 
 
 
+a,b='Hola','Adios'
+
+print(a,b)
+
+# swap(intercambio de variable)
+
+a,b=b,a
+
+print(a,b)
+
+# Regresar múltiples valores en una función
+
+def minmax(elementos):
+    return min(elementos),max(elementos)
+
+
+min,max=minmax([1,2,3,4,5])
+
+print(f"Valor min: {min}, Valor max: {max}")
+
+
+
+# Regresar la suma de una tupla
+
+res=sum((1,2,3,4,5))
+
+print(f"El resultado final es: {res}")
+
+def suma(*args):
+    return sum(args)
+
+
+suma=suma(1,2,3,4,5)
+
+print(f"La suma final es: {suma}")
 
 
 
