4494bb47a4e641e30bd3654bfde4bc945fc21f19
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-08-17 11:02:05 -0300

    Closure en python, son otros tipos de funciones en python, tenemos funciones lamda,closure,principal,etc

---
+++ Python/Sección-33/Closure.py
@@ -0,0 +1,58 @@
+
+
+
+# Funciones closures en pythom, mantiene un estado, un closure es una función que defina a otra, y ademas la puede regresar, la función anidada puede acceder a las variables locales definidas en la función principal o externa.
+
+
+
+
+# Función principal
+
+# def operacion(a,b):
+#     # Definimos una función interna o anidada
+#     def sumar():
+#       return a+b
+
+#     # Retornar la función
+#     return sumar
+
+# Función principal
+def operacion(a,b):
+    # 1. Definimos una función lambda interna o anidada y la retornamos 
+    return lambda:a+b
+
+
+mi_funcion=operacion(3,3)
+
+print(f"Resultado: {mi_funcion()}")
+
+
+# Llamar la función regresada al vuelo
+
+print(f"Resultado de la funcion closure al vuelo: {operacion(1,1)()}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
