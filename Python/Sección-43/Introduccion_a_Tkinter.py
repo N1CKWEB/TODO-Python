a490ca34195432ea49159bdd9592cace2f50afe8
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-08-29 22:20:03 -0300

    Aprendiendo GUI con la libreria tkinter

---
+++ Python/Sección-43/Introduccion_a_Tkinter.py
@@ -0,0 +1,58 @@
+
+
+# Introducción  a Tkinter
+
+
+# GUI - Graphical User Interface
+
+# Tkinter - TK Interface
+
+# Importamos el modulo de tkinter
+import tkinter as tk
+# Importamos el módulo del tema de tkinter
+from tkinter import ttk
+
+# Creamos un objeto usando la clase TK
+ventana=tk.Tk()
+
+# Modificamos el tamaño de nuestra ventana (pixeles)
+ventana.geometry('600x400')
+
+# Cambiamos el nombre de la ventana
+ventana.title('Primera ventana con Tkinter!!!')
+
+
+# Configuramos el icono de la aplicación
+
+ventana.iconbitmap('C:/Users/LENOVO/OneDrive/Documentos/Ruta de Python Developer/Python/Sección-43/icono.ico')
+
+
+# Creamos un método
+def evento_click():
+    boton1.config(text='Boton presionado')
+    print('Ejecucion del evento click')
+    # Creamos un nuevo componente y los mostramos
+    boton2=ttk.Button(ventana,text='Nuevo boton')
+    boton2.pack()
+    
+
+# Creamos un boton (widget), el objeto padre es la ventana
+boton1=ttk.Button(ventana,text='Dar Click',command=evento_click)
+
+
+# Utilizar el pack layout manager para mostrar el botón de la ventana
+boton1.pack()
+
+
+# Inicializamos la ventana (esta línea la ejecutamos al final)
+# Si la ejecutamos antes, no se van a mostrar los cambiops anteriores
+ventana.mainloop() #Con este metodo mostramo la ventana, si o si tenes que usar este método para visualizar la ventana 
+
+
+
+
+
+
+
+
+
