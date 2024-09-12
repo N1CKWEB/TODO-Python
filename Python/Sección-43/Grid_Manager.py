de269de9b6ea2bb5d7afc21917a0485076904cdb
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-09-12 14:34:29 -0300

    Aprendiendo como hacer el grid y el tema del padding de los botones

--- Python/Sección-43/Grid_Manager.py
+++ Python/Sección-43/Grid_Manager.py
@@ -50,6 +50,7 @@ boton1=tk.Button(ventana,text='Boton 1',command=evento_uno)
 # Manejo de grid, digamos para colocar la posiciones de los botones donde queramos en la pantalla
 
 # sticky=pegajoso (se queda ifjo en uno solo lugar)
+# i = inner que seria un espacio que dejamos de manera interna para el pady o el ipady para el padding en tkinter
 boton1.grid(row=0,column=0,sticky='NSWE',padx=40, pady=30,ipadx=20,ipady=50,columnspan=2,rowspan=2)
 # boton2.grid(row=1,column=0,sticky='NSWE')
 # boton3.grid(row=0,column=1,sticky='NSWE')
@@ -59,7 +60,6 @@ boton1.grid(row=0,column=0,sticky='NSWE',padx=40, pady=30,ipadx=20,ipady=50,colu
 ventana.mainloop()
 
 
-# i = inner que seria un espacio que dejamos de manera interna para el pady o el ipady para el padding en tkinter
 
 
 
