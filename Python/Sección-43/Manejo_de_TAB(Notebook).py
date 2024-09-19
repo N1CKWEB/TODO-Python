0250ca872b4f052d10572895955a9db78dc97e90
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-09-19 11:44:08 -0300

    Volviendo a programar ahora a meterle a full, seguimos viendo tkinter con nuevos componentes

--- Python/Sección-43/Manejo_de_TAB(Notebook).py
+++ Python/Sección-43/Manejo_de_TAB(Notebook).py
@@ -1,6 +1,6 @@
 
 import tkinter as tk
-from tkinter import ttk,messagebox
+from tkinter import ttk,messagebox,scrolledtext
 
 # Manejo de Tabuladores (Notebook) en Tkinter
 
@@ -13,8 +13,45 @@ ventana.geometry('600x400')
 ventana.iconbitmap('C:/Users/LENOVO/OneDrive/Documentos/Ruta de Python Developer/Python/Sección-43/icono.ico')
 
 
-
-
+def crear_componentes_del_tabulador(tabulador):
+   # Agregamos una etiqueta y un componente de entrada
+  etiqueta1=ttk.Label(tabulador,text='Nombre: ')
+  etiqueta1.grid(row=0,column=0,sticky=tk.E)
+  entrada1=ttk.Entry(tabulador,width=30) 
+  entrada1.grid(row=0,column=1,padx=5,pady=5)  
+  
+  def enviar():
+      messagebox.showinfo('Mensaje ', f"Nombre: {entrada1.get()}")
+  
+  # Agregamos un botón
+  boto1=ttk.Button(tabulador,text='Enviar',command=enviar)
+  boto1.grid(row=1,column=0,columnspan=2)
+ 
+def crear_componente_tabulador3(tabulador):
+  
+  contenido='Este es mi texto con en el contenido'
+  # Creamos el componente scrool, y siempre ahí que poner el wrap con tk.WORD para completar la palabra
+  scroll=scrolledtext.ScrolledText(tabulador,width=50,height=10,wrap=tk.WORD)
+  scroll.insert(tk.INSERT,contenido)
+  # Mostramos el componente
+  scroll.grid(row=0,column=0)
+   
+def crear_componente_tabulador4(tabulador): 
+   
+  # Creamos una lista usando data list comprehensions
+  datos=[x+1 for x in range(10)]  
+  comboBox=ttk.Combobox(tabulador,width=15,values=datos)
+  comboBox.grid(row=0,column=0,padx=10,pady=10)
+  # Selecciona un elemento por default o mostrar
+  comboBox.current(1)
+   # Agregamos un boton para saber que opción seleccionó el usuario
+  def mostrar_valor():
+     messagebox.showinfo('Mostrar valor seleccionado',f"Valor seleccionado: {comboBox.get()}")  
+     
+  boton2=ttk.Button(tabulador,text='Mostrar valor seleccionado',command=mostrar_valor) 
+  boton2.grid(row=0,column=1)
+  
+  
 def crear_tabs():
   # Creamos un tab central, para ello usamos la clase de Notebook 
   control_tabulador=ttk.Notebook()
@@ -27,20 +64,22 @@ def crear_tabs():
   # Mostramos el tabulador
   control_tabulador.pack(fill='both') 
   
-  # Agregamos una etiqueta y un componente de entrada
-  etiqueta1=ttk.Label(tabulador_1,text='Nombre: ')
-  etiqueta1.grid(row=0,column=0,sticky=tk.E)
-  entrada1=ttk.Entry(tabulador_1,width=30) 
-  entrada1.grid(row=0,column=1,padx=5,pady=5)  
+  # Creamos los componentes del  tabulador 1
+  crear_componentes_del_tabulador(tabulador_1) 
+  # Creamos un segundo tabulador 
+  tabulador_3=ttk.LabelFrame(control_tabulador,text='Contenido')
+  control_tabulador.add(tabulador_3,text='Tabulador 3')
   
-  def enviar():
-      messagebox.showinfo('Mensaje ', f"Nombre: {entrada1.get()}")
+  # Creamos los componentes del segundo tabulador 3
+  crear_componente_tabulador3(tabulador_3)
+    
+  #Crear otro tabulador
+  tabulador_4=ttk.Labelframe(control_tabulador,text='Contenido')
+  control_tabulador.add(tabulador_4,text='Tabulador 4') 
+    
+  crear_componente_tabulador4(tabulador_4)  
+    
   
-  # Agregamos un botón
-  boto1=ttk.Button(tabulador_1,text='Enviar',command=enviar)
-  boto1.grid(row=1,column=0,columnspan=2)
-     
- 
 crear_tabs() 
 ventana.mainloop()
 
