c4490dc35146b07a484dbe4c241949acc7a4f3db
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-10-01 15:53:57 -0300

    Aprendiendo el manejo de tablas con python y tkinter

--- Python/Sección-47/Manejo_de_Tablas.py
+++ Python/Sección-47/Manejo_de_Tablas.py
@@ -1,14 +1,70 @@
 
 import tkinter as tk
-from tkinter import ttk,messagebox
+from tkinter import ttk,messagebox,scrolledtext
 
 
 # Manejo de tablas en python
-
-
 ventana=tk.Tk()
 ventana.title('Manejo de Tabla')
-ventana.geometry('700x400')
+ventana.geometry('600x400')
+ventana.configure(background='#1d2d44')
+ventana.resizable(0,0)
+
+
+
+# Configurar el grid
+
+ventana.columnconfigure(0,weight=1)
+ventana.columnconfigure(1,weight=0)
+# Definir un estilo
+
+estilo=ttk.Style()
+estilo.theme_use('clam') #preparar el manejo del tema oscuro
+estilo.configure('Treeview',
+                 background='black',
+                 foreground='white',
+                 fieldbackground='black',
+                 rowheight=30) 
+estilo.map('Treeview',background=[('selected','#3a86ff')])
+
+
+# Definir las columnas
+columnas=('Id','Nombre','Edad')
+tabla=ttk.Treeview(ventana,columns=columnas,show='headings')
+
+
+
+# Cabecera de la tabla
+tabla.heading('Id',text='Id',anchor=tk.CENTER)
+tabla.heading('Nombre',text='Nombre',anchor=tk.W)
+tabla.heading('Edad',text='Edad',anchor=tk.W)
+
+# Formato a las columnas 
+tabla.column('Id',width=80)
+tabla.column('Nombre',width=120)
+tabla.column('Edad',width=120)
+
+
+# Cargar datos a la tabla
+# datos=( (1,'Nicolas',19),
+#         (2,'Juan',19),
+#         (3,"Naim",19),
+#         (4,"Nacho",19) )
+
+
+datos= ((1,"Nicolas",19),(2,"Juan",19),(3,"Naim",19),(4,"Nacho",19)) + ((1,"Nicolas",19),(2,"Juan",19),(3,"Naim",19),(4,"Nacho",19)) + ((1,"Nicolas",19),(2,"Juan",19),(3,"Naim",19),(4,"Nacho",19)) + ((1,"Nicolas",19),(2,"Juan",19),(3,"Naim",19),(4,"Nacho",19)) + ((1,"Nicolas",19),(2,"Juan",19),(3,"Naim",19),(4,"Nacho",19)) + ((1,"Nicolas",19),(2,"Juan",19),(3,"Naim",19),(4,"Nacho",19))
+
+
+for persona in datos:
+    tabla.insert(parent='',index=tk.END,values=persona) 
+
+# Scroll de la tabla
+scroll=ttk.Scrollbar(ventana,orient=tk.VERTICAL,command=tabla.yview)
+tabla.configure(yscroll=scroll.set)
+scroll.grid(row=0,column=1,sticky=tk.NS)
+
+#Publicar nuestra tabla
+tabla.grid(row=0,column=0,sticky=tk.NSEW) 
 
 
 ventana.mainloop()
