a1ae0d5beb04a7fd1a9599ef20ae25b0ad1bbbce
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-10-01 15:19:28 -0300

    Terminando de realizar app de login con modo oscuero

---
+++ Python/Sección-47/Manejo_de_Tablas.py
@@ -0,0 +1,14 @@
+
+import tkinter as tk
+from tkinter import ttk,messagebox
+
+
+# Manejo de tablas en python
+
+
+ventana=tk.Tk()
+ventana.title('Manejo de Tabla')
+ventana.geometry('700x400')
+
+
+ventana.mainloop()
