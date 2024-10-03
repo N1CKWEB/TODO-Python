4abc80bd47ac93616a92c433846c1ec3fc90eb20
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-10-03 17:23:19 -0300

    Realizando la GUI ahora para vincular después todo

--- Python/Sección-48/AppZonaFit.py
+++ Python/Sección-48/AppZonaFit.py
@@ -1,3 +1,58 @@
 
 
 # Clase de AppZonaFit donde agregaremos todos los metodos 
+import tkinter as tk
+from tkinter import ttk,messagebox
+
+
+
+class App(tk.Tk):
+       
+       
+       
+       def __init__(self):
+           super().__init__()
+           self.configurar_ventana()
+           self.configurar_grid()
+           
+        # #  Mostrar la tabla
+        #    self.mostrar_tablas()
+           
+       def configurar_ventana(self):
+               
+            self.title('Zona Fit App')          
+            self.geometry('600x400')
+            self.configure(background='#1d2d44')
+            self.resizable(0,0)
+            
+       def configurar_grid(self):     
+            self.columnconfigure(0,weight=1)
+            self.columnconfigure(1,weight=0)
+            
+       def mostrar_tabla(self):     
+           pass
+
+
+
+if __name__ == "__main__":
+    appFit=App()
+    appFit.mainloop()
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
