244812176270f81402ac52cf6df042a0513997ab
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-10-03 16:21:48 -0300

    Agregando las clases de cliente y base de datos conectada ya

--- Python/Sección-48/Cliente.py
+++ Python/Sección-48/Cliente.py
@@ -1,6 +1,59 @@
 
-
+from Logger_base import log
 
 # Clase de cliente donde haremos todos sus metodos
 
- 
+class Cliente:
+    
+    
+    @classmethod
+    
+    
+    def __init__(self,id_cliente ,nombre, apellido,membresia):
+      self._id_cliente=id_cliente
+      self._nombre = nombre
+      self._apellido = apellido
+      self._membresia = membresia
+      
+    
+    @property
+    def id_cliente(self):
+        return self._id_cliente  
+    
+    @id_cliente.setter
+    def nombre(self,id_cliente):
+        self._id_cliente=id_cliente
+    
+    @property
+    def nombre(self):
+        return self._nombre  
+    
+    @nombre.setter
+    def nombre(self,nombre):
+        self._nombre=nombre
+    
+    @property
+    def apellido(self):
+        return self._apellido
+    
+    @apellido.setter
+    def apellido(self,apellido):
+        self._apellido=apellido
+    
+    @property
+    def membresia(self):
+        return self._membresia
+           
+    @membresia.setter
+    def membresia(self,membresia):
+        self._membresia=membresia
+    
+   
+    def __str__(self) -> str:
+        return f"Cliente [ID: {self._id_cliente} Nombre: {self._nombre} , Apellido: {self._apellido} , Membresia: {self._membresia}]"
+    
+                
+if __name__ == "__main__":
+    usuario02 = Cliente(1,"Nicolas","Diaz","2303")
+    log.debug(usuario02)
+            
--- Python/Sección-48/ClienteDAO.py
+++ Python/Sección-48/ClienteDAO.py
@@ -2,7 +2,62 @@
 
 # Clase de clienteDAO donde agregaremos el CRUD o ALTA y BAJA y MODIFICADA
 
+from Conexión import Conexion
+from Cursor_del_pool import CursorDelPool
+from Cliente import Cliente
+from Logger_base import log
 
 
+class ClienteDAO():
 
 
+   _SELECCIONAR="SELECT * FROM cliente ORDER BY id_cliente"
+   _INSERTAR="INSERT INTO Cliente(nombre,apellido,membresia) VALUES ( %s , %s , %s)"
+   _ACTUALIZAR="UPADTE Cliente SET nombre=%s , apellido=%s,  membresia = %s WHERE id_cliente=%s"
+   _ELIMINAR="DELETE FROM Cliente WHERE id_cliente=%s"
+  
+  
+   # Seleccionar BD
+   
+   @classmethod
+   def seleccionar_bd(cls):
+       with CursorDelPool() as cursor:
+           cursor.execute(cls._SELECCIONAR)
+           registros=cursor.fetchall()
+           clientes=[]
+           for registro in registros:
+                cliente=Cliente(registro[0],registro[1],registro[2])
+                clientes.append(cliente) 
+           return clientes
+       
+    # Insertar BD
+    
+   @classmethod
+   def insertar_bd(cls,cliente):   
+       with CursorDelPool() as cursor:
+           valores=(cliente._nombre,cliente._apellido,cliente._membresia)
+           cursor.execute(cls._INSERTAR,valores)
+           log.debug(f"Cliente insertado: {cliente}")
+           return cursor.rowcount
+       
+    # Actualizar BD
+    
+   @classmethod
+   def actualizar_bs(cls,cliente):
+       with CursorDelPool() as cursor:
+            valores=(cliente._nombre,cliente._apellido,cliente._membresia)
+            cursor.execute(cls._ACTUALIZAR,valores)
+            log.debug(f"Cliente actualizado: {cliente}")   
+            return cursor.rowcount
+        
+        
+    # Eliminar BD
+   @classmethod
+   def eliminar_bd(cls,cliente):
+       with CursorDelPool() as cursor:
+         cursor.execute(cls._ELIMINAR, (cliente._id_persona))
+         log.debug(f"Persona eliminada con id: {cliente._id_cliente}")  
+         return cursor.rowcount
+     
+     
+     
